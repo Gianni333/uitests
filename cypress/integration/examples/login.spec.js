03f234ba62e603a4eebebe27e586a98ba213197c
Gianni333 <giannirhino@yahoo.co.uk>
2021-05-02 11:02:27 +0100

    Added more login tests

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -1,4 +1,13 @@
 describe("login", () => {
+    it("fails to authenticate", () => {
+      cy.visit("/index.php?controller=authentication&back=my-account");
+      cy.get("#email").type("sfewfew@yahoo.com"),
+      cy.get("#passwd").type("ewfewfwe"),
+      cy.get("#SubmitLogin").click(),
+      cy.get("div[class='alert alert-danger']").should('be.visible'),
+      cy.log('Authentication failed')
+    });
+
     it("logs in successfully", () => {
         cy.visit("/index.php?controller=authentication&back=my-account");
         cy.get("#email").type("giannirhino@yahoo.co.uk"),
@@ -7,4 +16,10 @@ describe("login", () => {
         cy.get("a[class='logout']").should('be.visible'),
         cy.log('Logout button visible')
       });
+
+    it("logs out successfully", () => {
+      cy.get("a[class='logout']").click(),
+      cy.get("#SubmitLogin").should('be.visible'),
+      cy.log('Login button visible')
+    });
 });
